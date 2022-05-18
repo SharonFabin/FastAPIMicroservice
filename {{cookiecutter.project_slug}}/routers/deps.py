643abe51d62192a1a68d92be3ea1b4dcdbd4a4e1
Sharon Fabin <i.sharon.fabin@gmail.com>
2022-05-18 12:25:45 +0300

    meow4

--- {{cookiecutter.project_slug}}/routers/deps.py
+++ {{cookiecutter.project_slug}}/routers/deps.py
@@ -1,26 +1,33 @@
+{%+ if cookiecutter.service_pattern_template == True +%}
 from services.service_example import ServiceInterface, ServiceExample
-from services.messaging import MessageProducer
+{%+ endif +%}
+{%+ if cookiecutter.message_technology == 'Kafka' +%}
+from services.messaging import MessageProducer, KafkaMessageProducer
+{%+ endif +%}
+{%+ if cookiecutter.message_technology == 'RabbitMQ' +%}
+from services.messaging import MessageProducer, RabbitMQProducer
+{%+ endif +%}
 
+
+{%+ if cookiecutter.service_pattern_template == True +%}
 service_example = ServiceExample()
-
-{%- if cookiecutter.messaging_technology == "Kafka" -%}
-from services.messaging import KafkaMessageProducer
-message_producer = KafkaMessageProducer()
-{%- elif cookiecutter.messagin_technology == "RabbitMQ" -%}
-from services.messaging import RabbitMQProducer
-message_producer = RabbitMQProducer()
-{% endif %}
-
-
-{%- if cookiecutter.messaging_technology != "None" -%}
-def get_message_producer() -> MessageProducer:
-    return message_producer
-{% endif %}
-
-
-{%- if cookiecutter.using_websocket == true -%}
 def get_service_example() -> ServiceInterface:
     return service_example
-{% endif %}
+{%+ endif +%}
+
+
+{%+ if cookiecutter.messaging_technology == "Kafka" +%}
+from services.messaging import KafkaMessageProducer
+message_producer = KafkaMessageProducer()
+{%+ elif cookiecutter.messagin_technology == "RabbitMQ" +%}
+from services.messaging import RabbitMQProducer
+message_producer = RabbitMQProducer()
+{%+ endif +%}
+{%+ if cookiecutter.messaging_technology != "None" +%}
+def get_message_producer() -> MessageProducer:
+    return message_producer
+{%+ endif +%}
+
+
 
 
