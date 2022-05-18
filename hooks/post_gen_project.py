15ec9a6ca924799f23eee74e0bc846a7c58496bf
Sharon Fabin <i.sharon.fabin@gmail.com>
2022-05-18 09:35:22 +0300

    message_queue

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,8 +1,24 @@
 from pathlib import Path
+import os
+import shutil
 
 
-path: Path
-for path in Path(".").glob("**/*.sh"):
-    data = path.read_bytes()
-    lf_data = data.replace(b"\r\n", b"\n")
-    path.write_bytes(lf_data)
+# path: Path
+# for path in Path(".").glob("**/*.sh"):
+#     data = path.read_bytes()
+#     lf_data = data.replace(b"\r\n", b"\n")
+#     path.write_bytes(lf_data)
+
+REMOVE_PATHS = [
+    '{% if cookiecutter.messaging_technology != "Kafka" %} services/messaging/kafka_message_producer.py {% endif %}',
+    '{% if cookiecutter.messaging_technology != "RabbitMQ" %} services/messaging/rabbitmq_message_producer.py {% endif %}',
+    '{% if cookiecutter.using_websocket != true %} utils/__init__.py {% endif %}',
+]
+
+for path in REMOVE_PATHS:
+    path = path.strip()
+    if path and os.path.exists(path):
+        if os.path.isdir(path):
+            os.rmdir(path)
+        else:
+            os.unlink(path)
