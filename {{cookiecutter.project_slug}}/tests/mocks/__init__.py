3daf3d1dd0f1dc6b8df7aea19aaed91b2e1b79d9
Sharon Fabin <i.sharon.fabin@gmail.com>
2022-05-18 13:55:20 +0300

    meow

--- {{cookiecutter.project_slug}}/tests/mocks/__init__.py
+++ {{cookiecutter.project_slug}}/tests/mocks/__init__.py
@@ -1,3 +1,12 @@
-# from .message_producer_mock import MessageProducerMock
+{%- if cookiecutter.messaging_technology != 'None' %}
+from .message_producer_mock import MessageProducerMock
+{%+ endif +%}
+{%- if cookiecutter.messaging_technology == 'Kafka' %}
+from .kafka_client import KafkaClient
+{%- elif cookiecutter.messaging_technology == 'RabbitMQ' %}
+from .rabbitmq_client import RabbitMQClient
+{%+ endif +%}
+{%- if cookiecutter.service_pattern_template == 'True' %}
 # from .service_mock import ServiceMock
-# from .kafka_client import KafkaClient
+{%+ endif +%}
+
---
+++ {{cookiecutter.project_slug}}/tests/mocks/rabbitmq_client.py
@@ -0,0 +1,18 @@
+import logging
+
+
+class RabbitMQClient():
+    def __init__(self):
+        pass
+
+    async def connect(self, ip: str, topic: str):
+        pass
+
+    async def consume_messages(self):
+        pass
+
+    def stop_consuming(self):
+        pass
+
+    async def close(self):
+        pass
