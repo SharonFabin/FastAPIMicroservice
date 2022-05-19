a810a2a18f5e6c064d3ee9b36cbd616cfae8a672
Sharon Fabin <i.sharon.fabin@gmail.com>
2022-05-19 18:58:46 +0300

    meow

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -2,11 +2,8 @@ import subprocess
 import shutil
 import os
 yaml_path = '{{ cookiecutter.openapi_path }}'
-shutil.copyfile(yaml_path, f'{os.getcwd()}/petstore.yaml')
+current_directory = os.getcwd()
+shutil.copyfile(yaml_path, f'{current_directory}/openapi.yaml')
 with open("/tmp/output.log", "a") as output:
-    subprocess.call("docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/petstore.yaml -g python-fastapi -o /local/out/python", shell=True, stdout=output, stderr=output)
-    # subprocess.call("docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/petstore.yaml -g python-fastapi -o /local/out/python", shell=True, stdout=output, stderr=output)
-
-# f = open("demofile2.txt", "a")
-# f.write("Now the file has more content!")
-# f.close()
+    subprocess.call("docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/openapi.yaml -g python-fastapi -o /local/out", shell=True, stdout=output, stderr=output)
+shutil.move(f'{current_directory}/tests/conftest.py', f'{current_directory}/conftest.py')
