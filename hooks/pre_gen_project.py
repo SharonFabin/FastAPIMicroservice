0b28dd6c653c0670e4f9d78fb909a0b9064eda0f
Sharon Fabin <i.sharon.fabin@gmail.com>
2022-05-19 11:18:36 +0300

    meow

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,4 +1,8 @@
 import subprocess
 
-with open("/tmp/output.log", "a") as output:
-    subprocess.call("docker run --rm -v $PWD:/local openapitools/openapi-generator-cli generate -i /local/petstore.yaml -g python-fastapi -o /local/out/python", shell=True, stdout=output, stderr=output)
+# with open("/tmp/output.log", "a") as output:
+#     subprocess.call("docker run --rm -v $PWD:/local openapitools/openapi-generator-cli generate -i /local/petstore.yaml -g python-fastapi -o /local/out/python", shell=True, stdout=output, stderr=output)
+
+f = open("demofile2.txt", "a")
+f.write("Now the file has more content!")
+f.close()
