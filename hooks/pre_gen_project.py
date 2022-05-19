f5b7d99c7c911a3b001141e47dcd37c6f561b129
Sharon Fabin <i.sharon.fabin@gmail.com>
2022-05-19 19:00:50 +0300

    meow

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -6,4 +6,4 @@ current_directory = os.getcwd()
 shutil.copyfile(yaml_path, f'{current_directory}/openapi.yaml')
 with open("/tmp/output.log", "a") as output:
     subprocess.call("docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/openapi.yaml -g python-fastapi -o /local/out", shell=True, stdout=output, stderr=output)
-shutil.move(f'{current_directory}/tests/conftest.py', f'{current_directory}/conftest.py')
+# shutil.move(f'{current_directory}/tests/conftest.py', f'{current_directory}/conftest.py')
